e5b6ba078d327037a79f004ed75284243c4b3c65
Riccardo Di Bella <riccardodibella2001@gmail.com>
2026-08-27 18:20:57 +0200

    choose_tree interface

--- exact_fidelity_simulation.py
+++ exact_fidelity_simulation.py
@@ -854,7 +854,6 @@ def optimistic_search_policy(l: list[tuple[str, float]], thresh: float, model: P
             if len(candidate_trees) > 0:
                 break
     if len(candidate_trees) > 0:
-        chosen_tree = candidate_trees[-1]
         def extract_immediate_choices_from_tree(tree: Tree) -> list[tuple[str, str]]:
             if type(tree) == str:
                 return []
@@ -867,6 +866,14 @@ def optimistic_search_policy(l: list[tuple[str, float]], thresh: float, model: P
             to_return += extract_immediate_choices_from_tree(left)
             to_return += extract_immediate_choices_from_tree(right)
             return to_return
+
+        def choose_tree(param_inputs: list[tuple[str, float]], param_trees: list[Tree]) -> Tree:
+            assert len(param_trees) > 0
+            working_candidates: list[Tree] = param_trees.copy()
+            # working_candidates = sorted(working_candidates, key=lambda t: len(extract_immediate_choices_from_tree(t)), reverse=True)
+            return working_candidates[-1]
+        
+        chosen_tree = choose_tree(l, candidate_trees)
         direct_choices: list[tuple[str, str]] = extract_immediate_choices_from_tree(chosen_tree)
 
         to_return: list[tuple[int, int]] = []
@@ -1504,7 +1511,7 @@ def progressive_increase_main() -> None:
     threshold = 0.9
     model = PurificationModel.BIT_FLIP
     NUM_SAMPLES = 100
-    MAX_PAIRS = 14
+    MAX_PAIRS = 7
 
 
     direct_policies: list[PolicyFunction] = [
@@ -1548,7 +1555,7 @@ def progressive_increase_main() -> None:
         StratType.DIRECT,
     ]
     strat_max_test_pairs: list[int] = [
-        6,
+        7,
         10,
         14,
         14,
